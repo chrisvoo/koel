d97741eb0a67b6aac71c64eeacd5fe7c7ff9e02b
Christian Castelli <voodoo81people@gmail.com>
2024-09-22 18:47:03 +0200

    merged master

--- sync.sh
+++
@@ -1,5 +0,0 @@
-MEDIA_PATH=/media/christian/Data/Music
-
-inotifywait -rme move,close_write,delete --format "%e %w%f" $MEDIA_PATH | while read file; do
-  php /var/www/koel/artisan koel:sync "${file}"
-done
